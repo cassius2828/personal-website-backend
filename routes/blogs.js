79994472e7b6a21e7995a12910a019dd7c421d7e
Cassius <cassius.reynolds.dev@gmail.com>
2025-02-08 07:02:28 -0800

    updated multer to include a larger fieldsize so all images that are base64 can be processed properly

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const blogRouter = require("../controllers/blogs");
-const upload = require("multer")();
+const multer = require("multer");
+const upload = multer({
+  limits: { fieldSize: 25 * 1024 * 1024 },
+});
 const verifyToken = require("../middleware/verify-token");
 router.get("/", blogRouter.getAllBlogs);
 router.post("/new", verifyToken, upload.single("img"), blogRouter.postNewBlog);
